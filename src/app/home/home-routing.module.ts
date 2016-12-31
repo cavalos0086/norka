1232860ca5d20f51d09c0f25e891fe29c937b6bc
Cristian E. Avalos <cavalos0086@gmail.com>
2016-12-30 21:44:09 -0500

    sets home view as default path

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 
 const homeRoutes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent, }
 ];
 
 @NgModule({
