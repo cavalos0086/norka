ebf8d98f24bc3e64daa051c45b4e1594a41aa669
Cristian E. Avalos <cavalos0086@gmail.com>
2016-12-30 21:17:08 -0500

    adds contactme module

---
+++ src/app/contactme/contactme.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactmeComponent } from './contactme.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ContactmeComponent]
+})
+export class ContactmeModule { }
