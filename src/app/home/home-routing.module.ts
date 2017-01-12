5d64d1591c060304210e1b15ffdcd3168c9ebcd4
Cristian E. Avalos <cavalos0086@gmail.com>
2017-01-11 22:19:57 -0500

    redirects any unknown route to home module

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -5,7 +5,8 @@ import { HomeComponent } from './home.component';
 
 const homeRoutes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
+    {path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
