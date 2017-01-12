afb90b8951cc5299c837f7b3415dd5d7d7b61726
Cristian E. Avalos <cavalos0086@gmail.com>
2017-01-11 23:19:57 -0500

    moves unknown path redirect to app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const appRoutes: Routes = [];
+const appRoutes: Routes = [
+    {path: '**', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,7 +6,6 @@ import { HomeComponent } from './home.component';
 const homeRoutes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
-    {path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
