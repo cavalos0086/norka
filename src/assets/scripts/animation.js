516f411b08fdf903e63274f1586bfe6be654e941
Cristian E. Avalos <cavalos0086@gmail.com>
2017-01-12 19:00:45 -0500

    refactors progressbar creation functions

--- src/assets/scripts/animation.js
+++ src/assets/scripts/animation.js
@@ -1,27 +1,36 @@
 window.onload = function() {
 
-    var commonOptions = {
-        color: '#FFEA82',
-        trailColor: '#f4f4f4',
-        trailWidth: 1,
-        duration: 1400,
-        easing: 'bounce',
-        strokeWidth: 0.5,
-        from: {color: '#FFEA82', a:0},
-        to: {color: '#ED6A5A', a:1},
-        // Set default step function for all animate calls
-        step: function(state, circle) {
-            circle.path.setAttribute('stroke', state.color);
-        }
-    }
 
     function textValue(value) {
         return {
-            text: {value: value}
+            text: {value: value.toString()}
         }
     }
 
-    var sketch = new ProgressBar.Circle('#sketch', Object.assign({}, commonOptions, textValue('0.93')));
-    sketch.animate(0.93);  // Number from 0.0 to 1.0
+    function createProgressBar(id, textvalue) {
+        var commonOptions = {
+            color: '#FFEA82',
+            trailColor: '#f4f4f4',
+            trailWidth: 1,
+            duration: 1400,
+            easing: 'bounce',
+            strokeWidth: 0.5,
+            from: {color: '#FFEA82', a:0},
+            to: {color: '#ED6A5A', a:1},
+            // Set default step function for all animate calls
+            step: function(state, circle) {
+                circle.path.setAttribute('stroke', state.color);
+            }
+        }
+
+        var options = Object.assign({}, commonOptions, textValue(0.93));
+        var progressBar = new ProgressBar.Circle(id.toString(), options);
+
+        return progressBar;
+    }
+
+    // var sketch = new ProgressBar.Circle('#sketch', Object.assign({}, commonOptions, textValue('0.93')));
+    createProgressBar('#sketch', '0.93').animate(0.93);  // Number from 0.0 to 1.0
+
 
 }
