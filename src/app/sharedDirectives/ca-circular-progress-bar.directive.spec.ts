cd214b6d711c6880c74b4329076cb4bb47d3f2f8
Cristian E. Avalos <cavalos0086@gmail.com>
2017-01-13 23:23:09 -0500

    adds circularProgressBar directive files to project

---
+++ src/app/sharedDirectives/ca-circular-progress-bar.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { CaCircularProgressBarDirective } from './ca-circular-progress-bar.directive';
+
+describe('CaCircularProgressBarDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CaCircularProgressBarDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/sharedDirectives/ca-circular-progress-bar.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appCaCircularProgressBar]'
+})
+export class CaCircularProgressBarDirective {
+
+  constructor() { }
+
+}
