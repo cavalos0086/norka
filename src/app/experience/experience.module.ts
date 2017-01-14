0bd5fa14d50f2960646fd65c76b52284539d4d42
Cristian E. Avalos <cavalos0086@gmail.com>
2017-01-13 23:50:18 -0500

    adds CaCircularProgressBarDirective to experience.module declarations

--- src/app/experience/experience.module.ts
+++ src/app/experience/experience.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ExperienceComponent } from './experience.component';
 import { ExperienceRoutingModule } from './experience-routing.module';
+import { CaCircularProgressBarDirective } from '../sharedDirectives/ca-circular-progress-bar.directive';
 
 @NgModule({
   imports: [
     CommonModule,
     ExperienceRoutingModule
   ],
-  declarations: [ExperienceComponent]
+  declarations: [
+      ExperienceComponent,
+      CaCircularProgressBarDirective
+  ]
 })
 export class ExperienceModule { }
