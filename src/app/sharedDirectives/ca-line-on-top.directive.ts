4254adb46772c068f1fe3d2f82c2024e7d913921
Cristian E. Avalos <cavalos0086@gmail.com>
2017-01-26 00:00:36 -0500

    implements ca-line-on-top directive and test it

--- src/app/sharedDirectives/ca-line-on-top.directive.ts
+++ src/app/sharedDirectives/ca-line-on-top.directive.ts
@@ -1,10 +1,19 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input, AfterContentInit } from '@angular/core';
 
 @Directive({
-  selector: '[appCaLineOnTop]'
+    selector: '[appCaLineOnTop]'
 })
-export class CaLineOnTopDirective {
+export class CaLineOnTopDirective implements AfterContentInit{
+    @Input() text: string;
+    element;
+    constructor(private el: ElementRef) {
+        this.element = el.nativeElement;
+    }
 
-  constructor() {}
+    ngAfterContentInit() {
+
+        // tslint:disable-next-line max-line-length
+        this.element.innerHTML = `<div class="container" style="margin-top:30px;"><div class="row"><div class="col-xs-2"><h3 style="border-top:3px solid black;text-align:right;">` + this.text + `</h3> </div> <div class="col-xs-10"></div></div>`;
+    }
 
 }
