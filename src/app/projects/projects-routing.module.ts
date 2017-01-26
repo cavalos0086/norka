ed024c1031a86597d2ce47eeaa8821592cc152c4
Cristian E. Avalos <cavalos0086@gmail.com>
2017-01-25 22:14:04 -0500

    adds a child route to project-routing

--- src/app/projects/projects-routing.module.ts
+++ src/app/projects/projects-routing.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProjectsComponent } from './projects.component';
+import { ProjectNavigationComponent } from './project-navigation/project-navigation.component';
 
-const routes: Routes = [
-    {path: 'projects', component: ProjectsComponent}
+const projectRoutes: Routes = [
+    {
+        path: 'projects',
+        component: ProjectsComponent,
+        children: [
+            {path: '', component: ProjectNavigationComponent}
+        ]
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(projectRoutes)],
   exports: [RouterModule],
   providers: []
 })
