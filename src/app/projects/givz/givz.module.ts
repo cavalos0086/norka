66a460718af2c86e2393e2791801449a862c4a9e
Cristian E. Avalos <cavalos0086@gmail.com>
2017-01-27 23:00:35 -0500

    adds shareDirectivesModule to GIZ module

--- src/app/projects/givz/givz.module.ts
+++ src/app/projects/givz/givz.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { GivzRoutingModule } from './givz-routing.module';
+import { SharedDirectivesModule } from '../../sharedDirectives/sharedDirectives.module';
 import { GivzComponent } from './givz.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedDirectivesModule,
     GivzRoutingModule
   ],
   declarations: [GivzComponent]
