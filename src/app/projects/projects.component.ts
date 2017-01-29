978943d257cb0ff2e8dbfd5a23735a43ba18ff79
Cristian E. Avalos <cavalos0086@gmail.com>
2017-01-29 15:15:33 -0500

    highlights div based on which project active

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,6 +8,9 @@ import { Router } from '@angular/router';
 })
 export class ProjectsComponent implements OnInit {
     projectsRoute;
+    isGivzActive = false;
+    isPomowebActive = false;
+    isStrandActive = false;
 
   constructor(private router: Router) {
       // store projects route to navigation to root later.
@@ -18,6 +21,24 @@ export class ProjectsComponent implements OnInit {
   }
 
   goTo(view: string) {
+      switch(view) {
+          case 'givz':
+            this.isGivzActive = true;
+            this.isPomowebActive = false;
+            this.isStrandActive = false;
+            break;
+        case 'pomoweb':
+            this.isGivzActive = false;
+            this.isPomowebActive = true;
+            this.isStrandActive = false;
+            break;
+        case 'strand':
+            this.isGivzActive = false;
+            this.isPomowebActive = false;
+            this.isStrandActive = true;
+            break;
+      }
+
       this.router.navigate([this.projectsRoute + '/' + view]);
   }
 }
