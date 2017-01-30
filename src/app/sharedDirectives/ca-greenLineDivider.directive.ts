bc99fece7a957bbdd149ff9a82b57e984d50e66a
Cristian E. Avalos <cavalos0086@gmail.com>
2017-01-29 21:36:55 -0500

    extends greenLineDivider directive to take margin values

--- src/app/sharedDirectives/ca-greenLineDivider.directive.ts
+++ src/app/sharedDirectives/ca-greenLineDivider.directive.ts
@@ -1,13 +1,20 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input, AfterContentInit } from '@angular/core';
 
 @Directive({
     selector: '[appCaGreenLineDivider]'
 })
 
-export class CaGreenLineDividerDirective {
+export class CaGreenLineDividerDirective implements AfterContentInit{
+
+    @Input() lineMargin: string;
     constructor(private el: ElementRef) {
         el.nativeElement.style.background = '#4ECDC4';
         el.nativeElement.style.height = '10px';
         el.nativeElement.style.opacity = '0.35';
+    }
+
+    // we use ngAfterContentInit to ensure that template has already been compiled at this point
+    ngAfterContentInit() {
+        this.el.nativeElement.style.margin = this.lineMargin;
     }
 }
